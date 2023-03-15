ace96e7d79e2a35481a54721528a0c34169e5b79
Benjamin Yurchison <yurchisonben@gmail.com>
2023-03-14 23:56:09 -0400

    init

---
+++ StageDive/StageDiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageDiveApp.swift
+//  StageDive
+//
+//  Created by Benjamin Yurchison on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct StageDiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
